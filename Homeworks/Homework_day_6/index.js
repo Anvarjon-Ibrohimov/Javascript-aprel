b2955010322cce435010711af95866d9d19f30c0
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-04-12 16:39:23 +0500

    day_6

---
+++ Homeworks/Homework_day_6/index.js
@@ -0,0 +1,53 @@
+// Task 1
+
+// let address = {
+//     street: 'Andijan',
+//     city: 'Namangan',
+//     zipcode: '190601'
+// }
+// // console.log(address);
+
+// function showAddress(address) {
+//     console.log(Object.entries(address));
+// }
+// showAddress(address)
+
+
+// task-2
+
+// let library = [{
+//         author: 'Bill Gates',
+//         title: 'The Road Ahead',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Stiv Jobs',
+//         title: 'Walter Isaccson',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Susanna Collins',
+//         title: 'The Mockingay:the final Book of The Hungry Games',
+//         readingStatus: false
+//     }
+// ]
+
+// for (let obj of library) {
+//     for (let item in obj) {
+//         console.log(obj[item]);
+//     }
+// }
+
+// task3
+
+
+// function old35(number) {
+//     if (number % 3 == 0 && number % 5 == 0) {
+//         return false;
+//     } else if (number % 3 == 0 || number % 5 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(old35(11));
