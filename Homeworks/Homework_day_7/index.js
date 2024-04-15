865de6519a5f3e7b5b8ff16b5f7b4b93db8a74a9
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-04-15 10:09:38 +0500

    homework

---
+++ Homeworks/Homework_day_7/index.js
@@ -0,0 +1,32 @@
+// Task_1
+
+// function Hello(name) {
+//     return `Hello ${name}`
+// }
+
+// console.log(Hello('Anvarjon'));
+// console.log(Hello('Ikromjon'));
+// console.log(Hello('Abdurahmon'));
+
+// task_2
+
+// let marks = [80, 80, 50];
+// let sum = 0;
+// for (let i = 0; i < marks.length; i++) {
+//     // console.log(marks[i]);
+//     sum += marks[i];
+// }
+// let averageMark = sum / marks.length;
+// if (averageMark >= 1 && averageMark < 60) {
+//     console.log('Your grade is F');
+// } else if (averageMark >= 60 && averageMark < 70) {
+//     console.log('Your grade is D');
+// } else if (averageMark >= 70 && averageMark < 80) {
+//     console.log('Your grade is C');
+// } else if (averageMark >= 80 && averageMark < 90) {
+//     console.log('Your grade is B');
+// } else if (averageMark >= 90 && averageMark <= 100) {
+//     console.log('Your grade is A');
+// } else {
+//     console.log('Your grade is none');
+// }
