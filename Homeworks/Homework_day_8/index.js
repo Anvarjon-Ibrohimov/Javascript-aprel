a828d48d7d01994310d095c121a9cf2e6880f3d8
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-04-17 11:42:12 +0500

    Task 6

---
+++ Homeworks/Homework_day_8/index.js
@@ -0,0 +1,25 @@
+// Task 1
+// String nomi berilgan, shu string ning oxirgi 2ta harfini boshiga olib qo'y. Agar string 2 ta dan iborat bo'lsa, o'zini qayatrsin
+
+
+// function right2(str) {
+//     if (str.length < 2) {
+//         return str;
+//     } else {
+//         return str.slice(2, str.length) + str[0] + str[1];
+//     }
+// }
+// console.log(right2('Hello'));
+// console.log(right2('java'));
+// console.log(right2('Hi'));
+
+
+// task2()
+
+// function makeabba(str1, str2) {
+//     return str1 + str2 + str2 + str1;
+// }
+
+// console.log(makeabba('Hi', 'Bye'));
+// console.log(makeabba('Yo', 'Alice'));
+// console.log(makeabba('what', 'Up'));
