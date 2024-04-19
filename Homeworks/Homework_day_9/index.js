ba0332c841126f781977f77f5afce5ff90c654f0
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-04-19 13:55:48 +0500

    homework

---
+++ Homeworks/Homework_day_9/index.js
@@ -0,0 +1,48 @@
+// function randomArticle(article) {
+//   const word = article.split(" ");
+
+//   console.log(word);
+
+//   const id = word.join("-").toLowerCase();
+//   console.log(id);
+// }
+
+// randomArticle(
+//   "«Rinotin kids» biologik faol qo‘shimchasiga berilgan ruxsatnoma bekor qilindi";
+// );
+
+// task 2
+
+// let words = "Telegram";
+// let letterText = words.split("");
+// console.log(letterText);
+
+// Homeworks task1
+
+// function arrayFromrange(min, max) {
+//   let array = [];
+//   for (let i = min; i <= max; i++) {
+//     array.push(i);
+//   }
+//   return array;
+// }
+
+// const numbers = arrayFromrange(1, 4);
+// const numbers2 = arrayFromrange(-10, -4);
+// console.log(numbers);
+// console.log(numbers2);
+
+// task 2
+
+// function includes(array, searchelem) {
+//   for (let item of array) {
+//     if (item == !searchelem) {
+//       return false;
+//       console.log(searchelem);
+//     } else {
+//       return true;
+//     }
+//   }
+// }
+
+// console.log(includes([4, 5, 6, 7, 8], 8));
